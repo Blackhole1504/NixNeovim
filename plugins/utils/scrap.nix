c6283f0e693cc94ff77622bcca88bd8aa3a72319
Blackhole1504 <51298403+Blackhole1504@users.noreply.github.com>
2023-01-27 18:53:43 +0100

    add scrap.nix

---
+++ plugins/utils/scrap.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+
+let
+
+  name = "scrap";
+  pluginUrl = "Mateiadrielrafael/scrap.nvim";
+
+  helpers = import ../helpers.nix { inherit lib config; };
+  cfg = config.programs.nixneovim.plugins.${name};
+
+  moduleOptions = with helpers; {
+    # add module options here
+    #
+    # autoStart = boolOption true "Enable this pugin at start"
+  };
+
+  # pluginOptions = {
+  #   # manually add plugin mapping of module options here
+  #   #
+  #   # auto_start = cfg.autoStart
+  # };
+
+  # you can autogenerate the plugin options from the moduleOptions.
+  # This essentially converts the camalCase moduleOptions to snake_case plugin options
+  pluginOptions = helpers.toLuaOptions cfg moduleOptions;
+
+in
+with helpers;
+mkLuaPlugin {
+  inherit name moduleOptions pluginUrl;
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    # nvim-treesitter
+  ];
+  extraPackages = with pkgs; [
+    # add dependencies here
+    # tree-sitter
+  ];
+  extraConfigLua = "require('${name}').setup ${toLuaObject pluginOptions}";
+  defaultRequire = true;
+}
