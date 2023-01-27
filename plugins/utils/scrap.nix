768cb1707344f7cc9a365d58839b2e0da68948bd
Blackhole1504 <51298403+Blackhole1504@users.noreply.github.com>
2023-01-27 19:04:48 +0100

    edit scrap.nix

--- plugins/utils/scrap.nix
+++ plugins/utils/scrap.nix
@@ -13,14 +13,14 @@ let
   moduleOptions = with helpers; {
     # add module options here
     #
-     autoStart = boolOption true "Enable this pugin at start";
+    autoStart = boolOption true "Enable this pugin at start";
   };
 
-   pluginOptions = {
-     # manually add plugin mapping of module options here
-     #
-      auto_start = cfg.autoStart;
-   };
+  # pluginOptions = {
+  #   # manually add plugin mapping of module options here
+  #   #
+  #   # auto_start = cfg.autoStart
+  # };
 
   # you can autogenerate the plugin options from the moduleOptions.
   # This essentially converts the camalCase moduleOptions to snake_case plugin options
