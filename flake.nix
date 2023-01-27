a3f630eaa67291329b2629edc53f67bb0962033d
Bastien <51298403+Blackhole1504@users.noreply.github.com>
2023-01-27 13:05:21 +0100

    Follow nixos-22.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A neovim configuration system for NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     nmd = {
       url = "gitlab:rycee/nmd";
