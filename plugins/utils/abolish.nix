df636d0996de81582b650544d27b2ac5616885a4
Blackhole1504 <51298403+Blackhole1504@users.noreply.github.com>
2023-01-29 17:20:52 +0100

    abolish

--- plugins/utils/abolish.nix
+++ plugins/utils/abolish.nix
@@ -1,12 +1,41 @@
-{ lib, pkgs, config, ... }@attrs:
-let
-  helpers = import ../helpers.nix { inherit lib config; };
-in
-with helpers; with lib;
-mkPlugin attrs {
-  name = "abolish";
-  description = "Enable abolish.vim";
-  extraPlugins = [ pkgs.vimExtraPlugins.vim-abolish ];
+#{ lib, pkgs, config, ... }@attrs:
+#let
+#  helpers = import ../helpers.nix { inherit lib config; };
+#in
+#with helpers; with lib;
+#mkPlugin attrs {
+#  name = "abolish";
+#  description = "Enable abolish.vim";
+#  extraPlugins = [ pkgs.vimExtraPlugins.vim-abolish ];
+#
+#  options = { };
+#}
 
-  options = { };
+{ pkgs, lib, config, ... }:
+
+with lib;
+
+let
+
+  name = "abolish";
+  pluginUrl = "https://github.com/tpope/vim-abolish";
+
+  helpers = import ../helpers.nix { inherit lib config; };
+
+  moduleOptions = with helpers; {
+    # add module options here
+  };
+
+in
+with helpers;
+mkLuaPlugin {
+  inherit name moduleOptions pluginUrl;
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    abolish-vim
+  ];
+  extraPackages = with pkgs; [
+    # add dependencies here
+    # tree-sitter
+  ];
 }
